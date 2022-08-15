5f0c2caff7b44a848d16a541d4d281f7ca3c05e2
Abdallah Herzallah <fa_herzallah@esi.dz>
2022-08-15 03:58:48 +0200

    change show items to FlatList

--- screens/Page.tsx
+++ screens/Page.tsx
@@ -1,38 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList} from 'react-native';
 
 const URL =
   'https://www.maiia.com/api/pat-public/hcd?limit=500&locality=75001-PARIS&page=0&speciality.shortName=pharmacie';
 
-
 const Page = ({}) => {
-    const [data, setData] = useState([])
-    const [totalLoaded, setTotalLoaded]= useState(0)
+  const [data, setData] = useState([]);
+  const [totalLoaded, setTotalLoaded] = useState(0);
 
-    useEffect(() => {
-        fetch(URL)
-            .then((response) => response.json())
-            .then((json) => {
-                setData(json.items)
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    },[URL])
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json.items);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [URL]);
 
-    useEffect(() => {
-        setTotalLoaded(data.length)
-    },[data])
+  useEffect(() => {
+    setTotalLoaded(data.length);
+  }, [data]);
 
-    return (
-        <View>
-          <Text>Nombre de résultat :</Text>
-          <Text>{totalLoaded}</Text>
-          {data.map((item) => (
-            <Text>{item.center.name}</Text>
-          ))}
-        </View>
-    );
-}
+  const renderItem = ({item}) => <Text>{item.center.name}</Text>;
+
+  return (
+    <View>
+      <Text>Nombre de résultat :</Text>
+      <Text>{totalLoaded}</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+};
 
 export default Page;
